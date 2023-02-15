aea0e03ef4d91d068d26ca293bf0044278546e7f
Carlos Fabiano Gomes Mafra <Cfgmafra@outlook.com>
2023-02-15 00:58:44 -0400

    Modulo de editar

--- src/app/Modulos/cadastrar/cadastrar.component.ts
+++ src/app/Modulos/cadastrar/cadastrar.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Service} from "../../Service/service.component";
 import {AlunosModel} from "../../Pagina/interface/alunos";
@@ -13,22 +13,23 @@ import {WebServiceService} from "../../Service/api/web-service.service";
   styleUrls: ['./cadastrar.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class CadastrarComponent implements OnInit {
-  public formGroup: FormGroup;
+export class CadastrarComponent implements OnInit , OnDestroy {
+  public formGroupAluno: FormGroup;
+  public formGroupResponsavel: FormGroup;
+  public formGroupEndereco: FormGroup;
+  public aluno: AlunosModel;
+  public endereco: EnderecoModel;
+  public informacao: InformacoesModel;
   public loading : boolean = false;
   public cadastro : boolean = false;
   public isChecked: boolean = false;
   public isButton: boolean = true;
-  public aluno: AlunosModel;
-  public endereco: EnderecoModel;
-  public informacao: InformacoesModel;
-  public pass: string;
+  public pass: AlunosModel;
   public cep: string = '';
   public logradouro: string = '';
   public numero: string = '';
   public cidade: string = '';
   public bairro: string = '';
-
 
   constructor(
     private armazem: Service,
@@ -41,18 +42,26 @@ export class CadastrarComponent implements OnInit {
   ngOnInit(): void {
     this.form();
     this.pass = this.storage.getData('aluno');
-    console.log(this.pass);
+    if(this.pass !== null){
+      console.log(this.pass.id)
+      this.listAluno(this.pass.id)
+    }
+  }
 
+  ngOnDestroy(): void {
+    this.storage.setData('aluno',null)
   }
 
   public form(): void{
-    this.formGroup = this.formBuilder.group({
+    this.formGroupAluno = this.formBuilder.group({
       nome:['',[Validators.required]],
       CPF:['',[Validators.required]],
-      nascAluno:['',[Validators.required]],
+      nascAluno:['',[Validators.required]]});
+    this.formGroupResponsavel = this.formBuilder.group({
       nomeResponsavel:['',[Validators.required]],
       telefoneResponsavel:['',[Validators.required]],
-      emailResponsavel:['',[Validators.required]],
+      emailResponsavel:['',[Validators.required]]});
+    this.formGroupEndereco = this.formBuilder.group({
       logradouro: ['', [Validators.required]],
       numero: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(5)]],
       cidade: ['', [Validators.required]],
@@ -62,7 +71,7 @@ export class CadastrarComponent implements OnInit {
   }
 
   public consultarCEP(){
-    this.cep = this.formGroup.get('cep')?.value;
+    this.cep = this.formGroupEndereco.get('cep')?.value;
     this.web.buscarCep(this.cep).then((res) =>{
       this.bairro = res.data.bairro;
       this.logradouro = res.data.logradouro;
@@ -71,84 +80,85 @@ export class CadastrarComponent implements OnInit {
   }
 
   public cadastroAluno(): any  {
-    if(this.pass === undefined){
-      this.cadastrar();
-      console.log("Nulo")
+    if(this.pass === null ){
+      // this.cadastrar();
+      console.log("Cadastrar")
     } else{
-      console.log("Não nulo")
+      console.log("Editar")
       this.editarAluno(this.aluno.id);
     }
   }
 
   public valueFormAluno(): any{
-    const aluno = this.formGroup.value as AlunosModel;
-    aluno.CPF = this.formGroup.get('CPF')?.value;
-    aluno.nome = this.formGroup.get('nome')?.value;
-    aluno.nascAluno = this.formGroup.get('nascAluno')?.value;
+    const aluno = this.formGroupAluno.value as AlunosModel;
+    aluno.CPF = this.formGroupAluno.get('CPF')?.value;
+    aluno.nome = this.formGroupAluno.get('nome')?.value;
+    aluno.nascAluno = this.formGroupAluno.get('nascAluno')?.value;
     return aluno;
   }
 
   public valueFormInformacoes():any{
-    const informacoes = this.formGroup.value as InformacoesModel;
-    informacoes.nomeResponsavel = this.formGroup.get('nomeResponsavel')?.value;
-    informacoes.telefoneResponsavel = this.formGroup.get('telefoneResponsavel')?.value;
-    informacoes.emailResponsavel = this.formGroup.get('emailResponsavel')?.value;
-    return informacoes
+    const informacoes = this.formGroupResponsavel.value as InformacoesModel;
+    informacoes.nomeResponsavel = this.formGroupResponsavel.get('nomeResponsavel')?.value;
+    informacoes.telefoneResponsavel = this.formGroupResponsavel.get('telefoneResponsavel')?.value;
+    informacoes.emailResponsavel = this.formGroupResponsavel.get('emailResponsavel')?.value;
+    return informacoes;
   }
 
   public valueFormEndereco():any{
-    const endereco = this.formGroup.value as EnderecoModel;
-    endereco.cep = this.formGroup.get('cep')?.value;
-    endereco.logradouro = this.formGroup.get('logradouro')?.value;
-    endereco.numero = this.formGroup.get('numero')?.value;
-    endereco.bairro = this.formGroup.get('bairro')?.value;
-    endereco.cidade = this.formGroup.get('cidade')?.value;
+    const endereco = this.formGroupEndereco.value as EnderecoModel;
+    endereco.cep = this.formGroupEndereco.get('cep')?.value;
+    endereco.logradouro = this.formGroupEndereco.get('logradouro')?.value;
+    endereco.numero = this.formGroupEndereco.get('numero')?.value;
+    endereco.bairro = this.formGroupEndereco.get('bairro')?.value;
+    endereco.cidade = this.formGroupEndereco.get('cidade')?.value;
     return endereco;
   }
 
   public addSegResponsavel(): void{
     this.isChecked = true;
     this.isButton = false;
-    this.formGroup.addControl(
+    this.formGroupResponsavel.addControl(
       "nomeSegResponsavel",new FormControl('',[Validators.required]),
     )
-    this.formGroup.addControl(
+    this.formGroupResponsavel.addControl(
       "telefoneSegResponsavel",new FormControl('',[Validators.required]),
     )
-    this.formGroup.addControl(
+    this.formGroupResponsavel.addControl(
       "emailSegResponsavel", new FormControl('',[Validators.required]),)
   }
 
   public removeSegResponsavel(): void{
     this.isChecked = false;
     this.isButton = true;
-    this.formGroup.removeControl("nomeSegResponsavel");
-    this.formGroup.removeControl("telefoneSegResponsavel");
-    this.formGroup.removeControl("emailSegResponsavel");
+    this.formGroupResponsavel.removeControl("nomeSegResponsavel");
+    this.formGroupResponsavel.removeControl("telefoneSegResponsavel");
+    this.formGroupResponsavel.removeControl("emailSegResponsavel");
   }
 
 
   public cadastrar(): void  {
     this.aluno = this.valueFormAluno();
-    console.log(this.aluno)
     this.loading = true;
     this.armazem.createCadastro(this.aluno).then(()=>{
       this.loading = false;
-      this.formGroup?.reset();
+      this.formGroupAluno?.reset();
     },error =>{
       this.loading = false;
     })
+    this. informacao = this.valueFormInformacoes();
     this.informacao.idAluno = this.aluno.id;
     this.armazem.createInformacoes(this.informacao).then(()=>{
       this.loading = false;
-      this.formGroup?.reset();
+      this.formGroupResponsavel?.reset();
     },error =>{
       this.loading = false;
     })
+    this.endereco = this.valueFormEndereco();
     this.endereco.idAluno = this.aluno.id;
     this.armazem.createEndereço(this.endereco).then(()=>{
       this.loading = false;
-      this.formGroup?.reset();
+      this.formGroupEndereco?.reset();
     },error =>{
       this.loading = false;
     })
@@ -156,12 +166,37 @@ export class CadastrarComponent implements OnInit {
 
 
   public editarAluno(id: string): void{
-    const aluno = this.formGroup.value as AlunosModel;
-    aluno.CPF = this.formGroup.get('CPF')?.value;
-    aluno.nome = this.formGroup.get('nome')?.value;
-    aluno.nascAluno = this.formGroup.get('nascAluno')?.value;
+    //   this.aluno = this.valueFormAluno();
+    //   this.loading = true;
+    //   this.armazem.createCadastro(this.aluno).then(()=>{
+    //     this.loading = false;
+    //     this.formGroupAluno?.reset();
+    //   },error =>{
+    //     this.loading = false;
+    //   })
+    //   this. informacao = this.valueFormInformacoes();
+    //   this.informacao.idAluno = this.aluno.id;
+    //   this.armazem.createInformacoes(this.informacao).then(()=>{
+    //     this.loading = false;
+    //     this.formGroupResponsavel?.reset();
+    //   },error =>{
+    //     this.loading = false;
+    //   })
+    //   this.endereco = this.valueFormEndereco();
+    //   this.endereco.idAluno = this.aluno.id;
+    //   this.armazem.createEndereço(this.endereco).then(()=>{
+    //     this.loading = false;
+    //     this.formGroupEndereco?.reset();
+    //   },error =>{
+    //     this.loading = false;
+    //   })
   }
 
+  public listAluno(id: string): void{
+    this.armazem.getAluno(id).valueChanges().subscribe(res =>{
+      this.aluno = res;
+    })
+  }
 
 
 }
